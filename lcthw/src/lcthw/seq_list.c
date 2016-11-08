d6e697d020944c2b50ca9a704054d56939fd088b
byelipk <byelipk@gmail.com>
2016-11-08 07:58:15 -0500

    Add sequential search pseudo code

--- lcthw/src/lcthw/seq_list.c
+++ lcthw/src/lcthw/seq_list.c
@@ -1,6 +1,23 @@
 #include <lcthw/seq_list.h>
 #include <lcthw/algos.h>
 
+// ALGORITHM  SequentialSearch(A[0..n - 1], K)
+//
+// Desc:   Implements sequential search with a search key as a sentinel.
+// Input:  An array A of n elements an a search key K
+// Output: The index of the first element in A[0..n - 1] whose
+//         value is equal to K or -1 if no such element is found.
+//
+// def SequentialSearch(A, K) do
+//   A[n] <- K
+//   i <- 0
+//   while A[i] != K do
+//     i <- i + 1
+//   end
+//   if i < n return i
+//   else return -1
+// end
+
 int List_sequential_search(int list[], int size, int key)
 {
   int i = 0;
